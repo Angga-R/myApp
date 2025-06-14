1a257e867403f0019a88f9af1b0ba454ec854bf8
Angga R <shalangga047@gmail.com>
2025-06-15 05:18:44 +0900

    set UI dashboard & all data-akun

--- src/services/page-service.js
+++ src/services/page-service.js
@@ -1,13 +1,17 @@
-function loadContent(id, file) {
+function loadContent(file, title) {
   fetch(file)
     .then((response) => {
-      if (!response.ok) throw new Error("HTTP error " + response.status);
+      if (!response.ok) throw new Error("Error : " + response.status);
       return response.text();
     })
     .then((data) => {
-      document.getElementById(id).innerHTML = data;
+      document.getElementById("content").innerHTML = data;
+      document.getElementById("main-title").innerText = title;
+      document.getElementById("page-position").innerText = title;
     })
     .catch((error) => {
       console.error("Error loading component:", error);
     });
 }
+
+window.onload(loadContent("dashboard.html", "Dashboard"));
